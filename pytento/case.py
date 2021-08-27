396ce4215daee93c21a5ddd59f281f2470d5d0d9
londonist_demir <demir@londonist.co.uk>
2021-08-27 09:17:21 +0300

    added new files

---
+++ pytento/case.py
@@ -0,0 +1,41 @@
+class TestCase():
+    """this is the main class that runs the test cases"""
+
+    def __init__(self):
+        pass
+
+    # this methos checks if a and b are equal
+    def assertEqual(self, a, b):
+        pass
+
+    # this method checks if a and b are not equal
+    def assertNotEqual(self, a, b):
+        pass
+
+    # checks if x is true
+    def assertTrue(self, x):
+        pass
+
+    # checks if x is false
+    def assertFalse(self, x):
+        pass
+
+    # checks if a is b
+    def assertIs(self, a, b):
+        pass
+
+    # checks if a is not b
+    def assertIsNot(self, a, b):
+        pass
+
+    # check if the value is none
+    def assertIsNone(self, x):
+        pass
+
+    # check if the value is not note
+    def assertIsNotNone(self, x):
+        pass
+
+    # checks if a is in b
+    def assertIsIn(self, a ,b):
+        pass
