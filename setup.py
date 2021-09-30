d3b1afe297ce7cecf98145d5f7b9ed9196b5c791
londonist_demir <demir@londonist.co.uk>
2021-09-30 16:56:21 +0300

    upped the version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'pytento',
   packages = ['pytento'],
-  version = '1.0.0',
+  version = '1.0.1',
   license='MIT',
   description = 'This is a staticly-typed testing framework',
   author = 'Demir Antay',
